165d5e85b9159824c26091b0b5d520c19c469ac1
zzyo <1977795940@qq.com>
2019-04-30 15:57:33 +0800

    引入jquery

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import _ from 'lodash';
+import j from 'jquery';
 
 function component() {
-  const element = document.createElement('div');
+  const element = j('<div></div>');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.html(_.join(['Hello','webpack'], ' '))
 
-  return element;
+  return element.get(0);
 }
 
 document.body.appendChild(component());
