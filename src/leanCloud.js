483e1b423bc3ec3c897ff3797c4d30d67f115ba8
zzyo <1977795940@qq.com>
2019-05-13 17:23:48 +0800

    点击注册后使用 LeanCloud API 注册

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -1,10 +1,35 @@
 import AV from 'leancloud-storage'
 
-var APP_ID = 你的ID
-var APP_KEY = 你的KEY
+var APP_ID = 'lzNJdaGM6sL7nczk8ECBBFdw-gzGzoHsz';
+var APP_KEY = 'EpWXxc5TtKBw4YCPuoiJOcu7';
 AV.init({
   appId: APP_ID,
   appKey: APP_KEY
 })
 
 export default AV
+export function signUp(username, password, successFn, errorFn){
+  // 新建 AVUser 对象实例
+  var user = new AV.User()
+  // 设置用户名
+  user.setUsername(username)
+  // 设置密码
+  user.setPassword(password)
+  // 设置邮箱
+  user.signUp().then(function (loginedUser) {
+    let user = getUserFromAVUser(loginedUser)
+    successFn.call(null, user)
+  }, function (error) {
+    errorFn.call(null, error)
+  })
+
+  return undefined
+
+}
+
+function getUserFromAVUser(AVUser) {
+  return {
+    id: AVUser.id,
+    ...AVUser.attributes
+  }
+}
