84f82e47cb1401e538751ce6c2a4ec79af1186e3
zzyo <1977795940@qq.com>
2019-05-13 11:18:37 +0800

    fix value again

--- src/App.js
+++ src/App.js
@@ -28,13 +28,21 @@ class App extends Component {
       <div className="App">
       <h1>我的待办</h1>
       <div className="inputWrapper">
-      <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)} />
+      <TodoInput content={this.state.newTodo}
+    onChange={this.changeTitle.bind(this)}
+    onSubmit={this.addTodo.bind(this)} />
     </div>
     <ol>
     {todos}
     </ol>
     </div>
   )
+  }
+  changeTitle(event){
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    })
   }
   addTodo(event){
     this.state.todoList.push({
