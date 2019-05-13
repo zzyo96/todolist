f9ac1bb080b18bea7ee3bcb585200fa37e3c8afd
zzyo <1977795940@qq.com>
2019-05-13 11:03:36 +0800

    添加 normalize.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
+import 'normalize.css'
 
 class App extends Component {
   constructor(props){
